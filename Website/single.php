87a84a27797f8b6070f05253e7028b8641a70505
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-06 14:58:54 +0200

    second commit

--- Website/single.php
+++
@@ -1,9 +0,0 @@
-<?php
-get_header();
-
-while (have_posts()) : the_post(); 
-    get_template_part('template-parts/content', 'single');
-endwhile;
-
-get_footer();
-?>
--- Website/template-parts/content.php
+++
@@ -1,6 +0,0 @@
-<article>
-    <?php wp_get_attachment_image('get_the_ID'); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-
-</article>
